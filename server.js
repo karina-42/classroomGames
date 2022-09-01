d052bf88d04d1c960aa05d728c92204565694d96
Angela Vegega <angela.vegega@gmail.com>
2022-09-01 21:21:43 +0900

    Added controller and routes files to display the home page

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const homeRoutes = require('./routes/home')
+// const lineRowRoutes = require('./routes/lineRow')
+
 require('dotenv').config()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-// app.get(('/', req, res) => {
-//   res.
-// })
+app.use('/', homeRoutes)
+// app.use('/lineRow', lineRowRoutes)
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`catch the server at port ${process.env.PORT}`);
